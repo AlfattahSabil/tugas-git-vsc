24a96c2cbe82928dc5aeba29aa1b4ba93e7fb4f8
alfattah <alfattahsabil869@gmail.com>
2024-02-02 23:09:36 +0700

    latihan 1

---
+++ main.js
@@ -0,0 +1,16 @@
+const sr = ScrollReveal ({
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    reset: true,
+})
+
+sr.reveal('.text',{delay: 200, origin: 'top'})
+sr.reveal('.heading',{delay: 200, origin: 'top'})
+sr.reveal('.wrapper',{delay: 200, origin: 'left'})
+sr.reveal('.wrapper-2',{delay: 200, origin: 'right'})
+sr.reveal('.galery',{delay: 200, origin: 'top'})
+sr.reveal('.shop-container .box',{delay: 200, origin: 'top'})
+sr.reveal('.reviews-container .box',{delay: 200, origin: 'top'})
+sr.reveal('.body-contact .container',{delay: 200, origin: 'top'})
+sr.reveal('.footer-col',{delay: 200, origin: 'bottom'})
